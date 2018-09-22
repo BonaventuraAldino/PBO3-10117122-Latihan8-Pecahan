16eb0ef1c349b3ad021d838a45130a0844c4b662
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-23 00:27:13 +0700

    PBO3-10117122-Latihan8-Pecahan

---
+++ src/pbo3/pkg10117122/latihan8/pecahan/PBO310117122Latihan8Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan8.pecahan;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * 
+ */
+public class PBO310117122Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+}
